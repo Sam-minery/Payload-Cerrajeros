17273420143a3722c925198cd1f272259835798a
Sam <sam@MacBook-Pro-de-Sam.local>
2025-05-12 17:57:01 +0200

    Maquetado Final V1

--- src/Header/Component.client.tsx
+++ src/Header/Component.client.tsx
@@ -33,8 +33,12 @@ export const HeaderClient: React.FC<HeaderClientProps> = ({ data }) => {
     <header className="relative z-20 px-4 md:px-4 -mt-4" {...(theme ? { 'data-theme': theme } : {})}>
       <div className="py-0 flex justify-between items-center">
         <Link href="/">
-          <div className="bg-white/20 backdrop-blur-sm rounded-full p-1 w-fit mt-4">
-            <Logo loading="eager" priority="high" className="text-black dark:text-white h-24 w-auto" />
+          <div className="bg-white/20 backdrop-blur-sm rounded-full p-0.5 w-fit mt-6 sm:mt-4">
+            <Logo 
+              loading="eager" 
+              priority="high" 
+              className="text-black dark:text-white !max-h-16 sm:!max-h-20 lg:!max-h-24" 
+            />
           </div>
         </Link>
         <HeaderNav data={data} />
